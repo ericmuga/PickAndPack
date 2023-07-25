928e8bcc0fbfd561327161e1415952a323c24411
Eric Muga <eric.muga@gmail.com>
2023-07-25 05:46:15 +0300

    added sp_code|sp_name on make prepacks4

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -246,7 +246,7 @@ $orderLines = LineResource::collection($query->paginate(15)->appends($request->a
 $sp_codes=Order::whereIn('order_no', $orderLines->pluck('order_no')->toArray())
                 // ->distinct()
                 ->where('shp_date', '>=', Carbon::today()->toDateString())
-                ->select('sp_code', DB::raw("CONCAT(sp_code,'|', sp_name) as sp_code_and_name'"))
+                ->select('sp_code', DB::raw("CONCAT(sp_code,'|', sp_name) as sp_code_and_name"))
                 ->get()->unique();
 
     $orders = Order::query()
