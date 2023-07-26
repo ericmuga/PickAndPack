c507059b994a54947def0482f26a02a71a974f4f
Eric Muga <eric.muga@gmail.com>
2023-07-26 12:49:39 +0300

    added unique and replaced distinct in preppacked orders

--- app/Http/Controllers/LinePrepackController.php
+++ app/Http/Controllers/LinePrepackController.php
@@ -37,7 +37,7 @@ class LinePrepackController extends Controller
          $orders=OrderResource::collection($prepackLines->pluck('order')->unique('order_no'));
          $sp_codes=OrderResource::collection($prepackLines->pluck('order')->unique('sp_code'));
 
-       $prepackBatches=LinePrepackResource::collection(LinePrepack::select('created_at','batch_no')->distinct()->orderByDesc('id')->get());
+       $prepackBatches=LinePrepackResource::collection(LinePrepack::select('created_at','batch_no')->orderByDesc('id')->get()->unique('batch_no'));
 
 
       return inertia('LinePrepacks/List', compact('prepackItems','previousInput','prepackLines','prepackBatches','orders','sp_codes'));
