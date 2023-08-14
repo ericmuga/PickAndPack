939a6358a85de5bedbbd37816a97adcc330dc28b
Eric Muga <eric.muga@gmail.com>
2023-08-14 10:18:16 +0300

    fixed bug on getPars in order model1

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -125,11 +125,11 @@ public function confirmation(Request $request)
        }
 
        $order=Order::firstWhere('order_no',$request->order_no);
-       if ($order->getParts()==$order->confirmations()->count())
-       {
-        $order->confirmed=true;
-        $order->save();
-       }
+    //    if ($order->getParts()==$order->confirmations()->count())
+    //    {
+    //     $order->confirmed=true;
+    //     $order->save();
+    //    }
 $this->index($request);
   }
 }
@@ -263,49 +263,6 @@ $sp_codes = DB::table('orders')
 
 
 
-public function pick(Request $request){
-
-
-    //this will return a view that allows scanning of a given pick
-
-   //get all orders belonging to a given pick
-    //    Pick::when('p$request->search);
-
-    $picks=Pick::select('pick_no')
-               ->when($request->has('search')&&$request->search!='',fn($q)=>$q->where('serial_no',$request->search))
-               ->where('pick_time','>=',Carbon::today()->addDay(-3)->toDateString())
-            //    ->orderByDesc('serial_no')
-               ->groupBy('pick_no')
-               ->paginate(15);
-
-    $lines=[];
-
-    if ($picks->count=1)
-    {
-      //get all line items for that pick
-      $orders=Order::query()
-                //    ->confirmed()
-                    // ->shipCurrent()
-                    ->whereIn('order_no',Pick::select('order_no')->where('pick_no',$picks->first()->pick_no)->get()->pluck('order_no'))
-                    ->get()
-                    ->pluck('order_no');
-
-    // dd(substr($picks->first()->pick_no,2,1));
-
-      $lines=LineResource::collection(Line::whereIn('order_no',$orders)
-                                         ->where('part',substr($picks->first()->pick_no,2,1))
-                                         ->orderBy('item_description')
-                                         ->get());
-    //   dd($lines->first());
-    }
-
-
-    $previous=($request->has('search'))?$request->search:'';
-
-// dd($picks);
-
-    return inertia('Orders/Pick',compact('picks','previous','lines'));
-}
 
 
 
